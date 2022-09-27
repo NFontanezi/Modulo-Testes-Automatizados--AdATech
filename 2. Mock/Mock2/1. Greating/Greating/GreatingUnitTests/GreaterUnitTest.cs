22f927aee5d389e180415fec1428785143ea004f
Natalia Fontanezi <natalia.fontanezi@gmail.com>
2022-09-27 20:26:54 -0300

    feat mock metho w/o class base

--- 2. Mock/Mock2/1. Greating/Greating/GreatingUnitTests/GreaterUnitTest.cs
+++ 2. Mock/Mock2/1. Greating/Greating/GreatingUnitTests/GreaterUnitTest.cs
@@ -1,17 +1,32 @@
 using Greating.Domain;
 using Shouldly;
-
+using Moq;
 namespace GreatingUnitTests
 {
     public class GreaterUnitTest : MockSystemTime
     {
      
 
-        [Fact]
+        [Fact] // com classeBase
         public void Greeting_Morning_ShouldReturnGoodMorning() 
         {
             var mock = GetTimeNowMock(7);
             var sut =  new Greater(mock);
+
+            var actual = sut.Greeting();
+
+            actual.ShouldBe("BomDia");
+        }
+
+        [Fact] //sem classe Base
+        public void Greeting_Morning_ShouldReturnGoodMorningSimple()
+        {
+            var mock = new Mock<ISystemService>();
+
+            mock.Setup(x => x.GetTimeNow())
+                .Returns(new DateTime(2002, 02, 02, 07, 22, 50));
+
+            var sut = new Greater(mock.Object);
 
             var actual = sut.Greeting();
 
